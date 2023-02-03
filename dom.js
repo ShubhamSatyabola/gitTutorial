7f0adf7e09eb8bb5dd92bd2c12f188089638bd32
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-02-03 13:49:28 +0530

    dom file commit

---
+++ dom.js
@@ -0,0 +1,29 @@
+//console.dir(document);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.images);
+//console.log(document.all);
+//console.log(document.all[24]);
+
+
+//getElementById
+//const main=(document.getElementById("main"));
+//console.log(main);
+//main.textContent="BOSS";
+const head = document.getElementById("header-title");
+head.textContent="THE BOSS";
+head.style.border="solid 5px black";
+
+//getElementByClass
+const items = document.getElementsByClassName("list-group-item");
+//console.log(items);
+items[0].innerHTML="<h1>I Am The Boss</h1>"
+items[0].style.border="dashed 5px blue"
+items[0].style.fontStyle="italic"
+const title = document.getElementsByClassName("title")
+//console.log(title)
+title[0].style.fontWeight='bold'
+title[0].style.color="green"
+
+
+
