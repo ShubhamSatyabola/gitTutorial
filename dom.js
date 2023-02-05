f64c0ef544b5e281d86aee36d292a22e1e686e14
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-02-05 08:17:11 +0530

    traversing through dom adding Html

--- dom.js
+++ dom.js
@@ -10,9 +10,9 @@
 //const main=(document.getElementById("main"));
 //console.log(main);            
 //main.textContent="BOSS";                                                      
-const head = document.getElementById("header-title");
-head.textContent="THE BOSS";        
-head.style.border="solid 5px black";
+//const head = document.getElementById("header-title");
+//head.textContent="THE BOSS";        
+//head.style.border="solid 5px black";
                        
 //getElementByClass                                                                          
 //const items = document.getElementsByClassName("list-group-item");
@@ -38,22 +38,57 @@ head.style.border="solid 5px black";
 //title[0].style.fontWeight='bold'
 //title[0].style.color="green"  
 
-//query selector all
+/*query selector all
 const items=document.querySelectorAll(".list-group-item")
 items[1].style.background="green"
 items[1].textContent="i am green"
 items[1].style.color="green"
 //items[2].remove()
-
-const odd = document.querySelectorAll("li:nth-child(odd)")
+*/
+/*const odd = document.querySelectorAll("li:nth-child(odd)")
 for(var i =0; i<odd.length;i++){
     odd[i].style.background="green"
 }
 const even = document.querySelectorAll("li:nth-child(even)")
 for(var i =0; i<even.length;i++){
     even[i].style.background="pink"
-}
+}*/
+// Traversing The DOM
+const items = document.querySelector('#items')
+items.parentNode.style.background='yellow' //can use .parentnode to access parent items
+// .parentelement is same as .parentNode use to manipulate the parent element 
 
+//childNode return node list including whitespaces linebreaks 
+//to overcome the problem we can use children
+//items.childNodes[0].style.background='black' 
+items.children[0].style.background='green'
+
+
+// firstchild and first element child / lastchild and last element child
+//items.firstElementChild.innerHTML='<h1>hello brother</h1>'
+//items.lastElementChild.innerHTML='<h1>hello brother again</h1>'
+//console.log(items.children)
   
+//nextsibling and nextelementsibling / previoussibling and previouselementsibling
+items.previousElementSibling.style.color='blue'
 
+// creating element and inserting them using Dom
+//create element
+const div = document.createElement('div')
+div.className='hello';
+div.id='hello1'
+div.setAttribute('title','Hello World')
+const divtext = document.createTextNode('Hello world');
+div.appendChild(divtext)
+const h1=document.querySelector('h1');
+document.querySelector('header .container').insertBefore(div, h1)
+div.style.fontSize=('50px')
 
+const newdiv = document.createElement('div')
+div.className='hello';
+div.id='hello2'
+const newdivtext = document.createTextNode('Hello world');
+newdiv.appendChild(newdivtext)
+const item=document.querySelector('.item');
+const section = document.querySelector('.contain')
+section.insertBefore(newdiv, item)
