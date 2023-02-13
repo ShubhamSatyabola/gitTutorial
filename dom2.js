eac8abdf6ea49ddcc5c70f3676b6de34669bce24
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-02-13 22:14:07 +0530

    remove from ui and localStorage

--- dom2.js
+++ dom2.js
@@ -2,6 +2,7 @@ var form = document.getElementById('addform');
 var ul = document.getElementById('list-group');
 
 form.addEventListener('submit', setlocalStorage)
+
 //localstorage.setitem
 function setlocalStorage(e){
     e.preventDefault();
@@ -17,8 +18,18 @@ function showOnScreen(data){
     var li = document.createElement('li')
     li.className = 'list-group-item'
     li.textContent = `${data.Name}  ${data.email}  ${data.phone}`
+    
+    const btn = document.createElement('button')
+    btn.className = 'btn btn-dark float-right Delete'
+    btn.appendChild(document.createTextNode('Delete'))
+    
+    btn.onclick = () => {
+        localStorage.removeItem(data.email)
+
+        ul.removeChild(li)
+    }
+    li.appendChild(btn)
+
     ul.appendChild(li)
 
 }
-
-
